8d4898b86a7978d1609e3f7cc742bd8eddb86105
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-05-18 17:45:47 +0900

    fix：create like

--- controller/like.go
+++ controller/like.go
@@ -5,6 +5,8 @@ import (
 	"giiku5/model"
 	"giiku5/supabase"
 	"strconv"
+
+	"github.com/google/uuid"
 )
 
 // いいね情報をデータベースに挿入
@@ -12,8 +14,16 @@ func CreateLike() {
 	supabase, _ := supabase.GetClient()
 
 	// 仮置き. 自身のIDと相手のIDの取得方法分かり次第修正.
-	my_user_id := 1
-	other_user_id := 2
+	my_user_id, id_err := uuid.Parse("3a54e201-9341-480b-b3a6-e8a538dfec6d")
+	if id_err != nil {
+		fmt.Println(id_err)
+		return
+	}
+	other_user_id, other_id_err := uuid.Parse("dc1f8f58-b6bf-424a-9809-7abdbec16781")
+	if other_id_err != nil {
+		fmt.Println(other_id_err)
+		return
+	}
 
 	row := model.CreateLike{
 		UserID:      my_user_id,
--- model/likes.go
+++ model/likes.go
@@ -1,24 +1,28 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Like struct {
 	ID          int       `json:"id"`
-	UserID      int       `json:"user_id"`       // いいねを押した人のuser_id
-	LikedUserID int       `json:"liked_user_id"` // いいねを押された人のuser_id
+	UserID      uuid.UUID `json:"user_id"`       // いいねを押した人のuser_id
+	LikedUserID uuid.UUID `json:"liked_user_id"` // いいねを押された人のuser_id
 	CreatedAt   time.Time `json:"created_at"`
 }
 
 // いいね情報を挿入
 type CreateLike struct {
-	UserID      int `json:"user_id"`
-	LikedUserID int `json:"liked_user_id"`
+	UserID      uuid.UUID `json:"user_id"`
+	LikedUserID uuid.UUID `json:"liked_user_id"`
 }
 
 // いいね情報を削除
 type DeleteLike struct {
 	ID          int       `json:"id"`
-	UserID      int       `json:"user_id"`
-	LikedUserID int       `json:"liked_user_id"`
+	UserID      uuid.UUID `json:"user_id"`
+	LikedUserID uuid.UUID `json:"liked_user_id"`
 	CreatedAt   time.Time `json:"created_at"`
 }
