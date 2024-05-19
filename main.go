d14eb221a2ff2bcef8f1aa555d911f371ae51c94
kou7306 <kouta.1206.y@icloud.com>
2024-05-19 13:46:50 +0900

    datafetch

--- main.go
+++ main.go
@@ -5,9 +5,9 @@ import (
 	"net/http"
 
 	"giiku5/api"
-	"giiku5/controller"
 	"giiku5/domain"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
@@ -52,17 +52,14 @@ func main() {
 	go hub.RunLoop()
 	r := mux.NewRouter()
 	r.HandleFunc("/getMessage/{conversationId}", api.GetMessage).Methods("GET")
-	r.HandleFunc("/getMatchingUser", api.GetMatchingUser).Methods("POST")
-    r.HandleFunc("/getUserData", api.GetUserData).Methods("POST")
-	r.HandleFunc("/ws/{conversationId}", NewWebsocketHandler(hub).handleWebSocket)
-	r.HandleFunc("/random-match", controller.Random_Match).Methods("POST")
-	log.Println("WebSocket server started on localhost:8080")
-	// CORS設定
-	// headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
-	// originsOk := handlers.AllowedOrigins([]string{"https://giiku5-frontend.vercel.app", "http://localhost:3000"})
-	// methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
+    r.HandleFunc("/getMatchingUser", api.GetMatchingUser).Methods("POST")
+    r.HandleFunc("/ws/{conversationId}", NewWebsocketHandler(hub).handleWebSocket)
+    log.Println("WebSocket server started on localhost:8080")
+    // CORS設定
+    headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+    originsOk := handlers.AllowedOrigins([]string{"https://giiku5-frontend.vercel.app","http://localhost:3000"})
+    methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
-	
-    http.ListenAndServe(":8080", r)
-
+	// サーバー起動
+	http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r))
 }
