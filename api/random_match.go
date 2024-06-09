af8598e57628de257beec48ac8c1e7da96dc6b22
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-06-10 01:41:29 +0900

    update：ランダムマッチ

--- api/random_match.go
+++ api/random_match.go
@@ -3,6 +3,7 @@ package api
 import (
 	"giiku5/model"
 	"giiku5/supabase"
+	"log"
 	"math/rand"
 	"net/http"
 	"time"
@@ -29,7 +30,12 @@ func RandomMatch(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	log.Println(len(users))
+	// マッチングしている相手は除外する
+	users = DeleteMatchingUser(user_id, users)
 
+	log.Println(len(users))
+	// ランダムマッチで表示する相手の人数
 	users_num := 5
 	if len(users) < users_num {
 		users_num = len(users)
@@ -46,4 +52,51 @@ func RandomMatch(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, random_users)
+}
+
+// マッチングしている相手を除外
+func DeleteMatchingUser(user_id string, users []model.UserRandomResponse) []model.UserRandomResponse {
+	supabase, _ := supabase.GetClient()
+
+	var IDs1 []model.GetMatchingUser
+	var IDs2 []model.GetMatchingUser
+	var IDs []model.GetMatchingUser
+
+	// user_id = user2_idとなる行からuser1_idを取得
+	err := supabase.DB.From("matches").Select("*").Eq("user2_id", user_id).Execute(&IDs1)
+	if err != nil {
+		log.Fatalf("Error fetching liked_user_id: %v", err)
+	}
+
+	// user_id = user1_idとなる行からuser2_idを取得
+	err = supabase.DB.From("matches").Select("*").Eq("user1_id", user_id).Execute(&IDs2)
+	if err != nil {
+		log.Fatalf("Error fetching liked_user_id: %v", err)
+	}
+
+	IDs = append(IDs1, IDs2...)
+
+	var MatchIDs []string
+
+	for _, match := range IDs {
+		if match.User1ID == user_id {
+			matchingUserID := match.User2ID
+			MatchIDs = append(MatchIDs, matchingUserID)
+		} else {
+			matchingUserID := match.User1ID
+			MatchIDs = append(MatchIDs, matchingUserID)
+		}
+	}
+	log.Println(MatchIDs)
+	// MatchIDsに一致するUserIDを持つusersを削除
+	for _, matchID := range MatchIDs {
+		for i := 0; i < len(users); i++ {
+			if users[i].UserID == matchID {
+				users = append(users[:i], users[i+1:]...)
+				i--
+			}
+		}
+	}
+
+	return users
 }
