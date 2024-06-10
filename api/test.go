08fa05dbae533210621af6bc54148fcbf59dbda8
kou7306 <kouta.1206.y@icloud.com>
2024-06-10 20:25:11 +0900

    test

--- api/test.go
+++ api/test.go
@@ -1,32 +1,32 @@
-package api
+// package api
 
-import (
-	"log"
-	"net/http"
-	"os"
+// import (
+// 	"log"
+// 	"net/http"
+// 	"os"
 
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/joho/godotenv"
+// )
 
-// ユーザーがマッチングしているか確認(互いにいいねしているか)
-func Test(c *gin.Context) {
+// // ユーザーがマッチングしているか確認(互いにいいねしているか)
+// func Test(c *gin.Context) {
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	supabaseUrl := os.Getenv("SUPABASE_URL")
-	supabaseKey := os.Getenv("SUPABASE_KEY")
-	log.Println(supabaseUrl)
-	log.Println(supabaseKey)
+// 	supabaseUrl := os.Getenv("SUPABASE_URL")
+// 	supabaseKey := os.Getenv("SUPABASE_KEY")
+// 	log.Println(supabaseUrl)
+// 	log.Println(supabaseKey)
 
 
-	// JSONデータを作成
-	jsonData := map[string]interface{}{
-		"message": supabaseUrl + supabaseKey,
-	}
-	// JSONデータをレスポンスとして返す
-	c.JSON(http.StatusOK, jsonData)
-}
+// 	// JSONデータを作成
+// 	jsonData := map[string]interface{}{
+// 		"message": supabaseUrl + supabaseKey,
+// 	}
+// 	// JSONデータをレスポンスとして返す
+// 	c.JSON(http.StatusOK, jsonData)
+// }
